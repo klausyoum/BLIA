1b0cdac83907b0a3bd9f92dbf5e9fc6343882b25
Klaus <klausyoum@gmail.com>
2015-07-26 10:54:10 +0900

    initializeDB() added.

--- src/edu/skku/selab/blp/BLP.java
+++ src/edu/skku/selab/blp/BLP.java
@@ -8,6 +8,8 @@
 package edu.skku.selab.blp;
 
 import edu.skku.selab.blp.blia.analysis.BLIA;
+import edu.skku.selab.blp.db.dao.BaseDAO;
+import edu.skku.selab.blp.db.dao.DbUtil;
 import edu.skku.selab.blp.evaluation.Evaluator;
 
 /**
@@ -15,6 +17,37 @@ import edu.skku.selab.blp.evaluation.Evaluator;
  *
  */
 public class BLP {
+	private static void initializeDB() throws Exception {
+		Property prop = Property.getInstance();
+		
+		DbUtil dbUtil = new DbUtil();
+		String productName[] = {
+				Property.ASPECTJ,
+				Property.ECLIPSE,
+				Property.SWT,
+				Property.ZXING};
+		
+		for (int i = 0; i < productName.length; i++) {
+			dbUtil.openConnetion(productName[i]);
+
+			dbUtil.dropAllAnalysisTables();
+			dbUtil.createAllAnalysisTables();
+
+			prop.setProductName(productName[i]);
+			dbUtil.initializeAllData();
+
+			dbUtil.closeConnection();
+		}
+		
+		dbUtil.openEvaluationDbConnection();
+
+		dbUtil.dropEvaluationTable();
+		dbUtil.createEvaluationTable();
+		
+		dbUtil.initializeExperimentResultData();
+		
+		dbUtil.closeConnection();
+	}
 
 	/**
 	 * @param args
@@ -22,6 +55,9 @@ public class BLP {
 	public static void main(String[] args) throws Exception {
 		// Load properties data to run BLIA
 		Property prop = Property.loadInstance();
+		
+		// initialize DB and create all tables.
+		initializeDB();
 
 		// Run BLIA algorithm
 		BLIA blia = new BLIA();
