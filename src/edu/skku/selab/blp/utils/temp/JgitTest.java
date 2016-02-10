2b8c6c73995595ca5a559e4a7076a86f5ca3d7c0
AhnJun <ahnjune881214@gmail.com>
2016-02-10 20:26:05 +0900

    - Update File : JgitTest.class Bug Repository to create a "JgitTest" development is complete. Add Method to bug Repository

--- src/edu/skku/selab/blp/utils/temp/JgitTest.java
+++ src/edu/skku/selab/blp/utils/temp/JgitTest.java
@@ -49,7 +49,6 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-//import jcodelib.util.CodeUtils;
 import edu.skku.selab.blp.utils.temp.MethodVisitor;
 import jcodelib.util.CodeUtils; 
 
